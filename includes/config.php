c2a1b8ca60076013d841d506b5f2c6ed2e80b175
James Johnson <james@mindsharelabs.com>
2017-07-14 09:46:52 -0600

    added gitignore

---
+++ includes/config.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: jameswjohnson
+ * Date: 2/22/17
+ * Time: 2:28 PM
+ */
---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: jameswjohnson
+ * Date: 2/22/17
+ * Time: 1:11 PM
+ */
